20a2ffa3c664a1b3beba5280479f592b87c83dc1
MoSmeha <smeha.sm3ha@gmail.com>
2025-11-12 23:05:25 +0200

    cleaned the code

--- cars-server/controllers/CarController.php
+++ cars-server/controllers/CarController.php
@@ -2,41 +2,21 @@
 require_once(__DIR__ . "/../models/Car.php");
 require_once(__DIR__ . "/../connection/connection.php");
 require_once(__DIR__ . "/../services/ResponseService.php");
-
+require_once(__DIR__ . "/../services/CarServices.php");
 class CarController {
 
     function getCars(){
         global $connection;
-        try {
             if(isset($_GET["id"])){
             $id = $_GET["id"];
-            $car = Car::find($connection, $id);
-            if(!$car){
-            echo ResponseService::response(404, "No car found with this id");
-            } else{
-            echo ResponseService::response(200, $car->toArray());
+            }else{
+                $id=null;
             }
-
-            return;
-            }
-            $allCars = Car::findAll($connection);
-            $carList = [];
-            foreach($allCars as $car){
-                $carList[] = $car->toArray();
-            }
-            echo ResponseService::response(200, $carList);
-            } catch(Exception $e) {
-                echo 'Error in fetching Cars : ' .$e->getMessage();
-            }
-       
-        // not allowed to write logic in my controller!!!
-
-        return;
-    }
-
-    //try catch 
-
+            echo CarService::getCars($connection,$id);
+            
+        }
 }
+
 $controller = new CarController();
 $controller->getCars();
 ?>
